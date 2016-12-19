2641470ac49a36ede073995caf2fba786355bd9e
@be <abeahmed2@gmail.com>
2016-12-19 15:00:36 -0500

    Initial commit.

---
+++ app/src/main/java/com/atbe/abe/topmovieslist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.atbe.abe.topmovieslist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
